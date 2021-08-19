0033425400c931af44d56aa3ab1629817357098f
Raymond <raymond@philippinedev.com>
2021-08-19 15:20:29 +0800

    add devise and buttons-functionality

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,34 @@
 class ContactsController < ApplicationController
+  before_action :set_contact, only: [:destroy]
   def index
+    @contacts = Contact.all
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to contacts_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    if @contact.destroy
+      redirect_to contacts_path
+    end
+  end
+
+  private
+  def contact_params
+    params.require(:contact).permit(:name, :number, :email)
+  end
+
+  def set_contact
+    @contact = Contact.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  resources :contacts
   root to: 'contacts#index'
 end
